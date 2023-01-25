329262ece94de57899a8595f8be9f1bed8a38eff
Di <dias.ospanov@gmail.com>
2023-01-25 20:34:30 +0200

    Add state functions.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 
-function addGuest(props) {
+const guestList = [
+  { firstName: 'Kevin', lastName: 'Spacey' },
+  { firstName: 'Brad', lastName: 'Pitt' },
+  { firstName: 'Sandra', lastName: 'Bullock' },
+];
+
+guestList.propTypes = {
+  guest: PropTypes.shape({
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+  }),
+};
+
+/* function addGuest(props) {
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <label>
       (props.fname) (props.lname)
-      <input type="radio" aria-checked="false" />
-      not attending
+      <input
+        checked={isChecked}
+        type="checkbox"
+        aria-checked="false"
+        onChange={(event) => setIsChecked(event.currentTarget.checked)}
+      />
+      {isChecked ? '' : 'not'} attending
     </label>
   );
-}
+} */
 
 function App() {
   const [addNewGuest, setAddNewGuest] = useState();
   const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <>
       <h1>Guest List</h1>
@@ -46,8 +67,13 @@ function App() {
         <div>
           Dias Ospanov
           <label>
-            <input type="radio" />
-            Attending
+            <input
+              checked={isChecked}
+              type="checkbox"
+              aria-checked="false"
+              onChange={(event) => setIsChecked(event.currentTarget.checked)}
+            />
+            {isChecked ? '' : 'not'} attending
           </label>
         </div>
       </div>
